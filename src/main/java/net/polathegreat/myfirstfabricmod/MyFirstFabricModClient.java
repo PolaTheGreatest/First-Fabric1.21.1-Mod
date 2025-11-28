dfb7b94a1ae53cacb189a5ef6fa447a1ff06cf57
PolaTheGreat <mostpersonalemailever@gmail.com>
2025-11-28 14:24:43 -0600

    Initial commit

---
+++ src/main/java/net/polathegreat/myfirstfabricmod/MyFirstFabricModClient.java
@@ -0,0 +1,10 @@
+package net.polathegreat.myfirstfabricmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstFabricModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
